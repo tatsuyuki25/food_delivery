d0f7dc870911f89b9d51a09933521a07912ea610
tatsuyuki <tatsuyuki@program.com.tw>
2023-04-04 21:25:08 +0800

    feat: 新增fake資料

--- lib/repository/restaurant_repository.dart
+++ lib/repository/restaurant_repository.dart
@@ -8,11 +8,17 @@ class Restaurant {
     required this.name,
     required this.description,
     required this.image,
+    required this.address,
     required this.rating,
+    required this.latitude,
+    required this.longitude,
   });
   final String id;
   final String name;
   final String description;
   final String image;
+  final String address;
   final double rating;
+  final double latitude;
+  final double longitude;
 }
